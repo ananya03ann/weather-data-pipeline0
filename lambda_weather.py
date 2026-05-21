c4f273133a81799b6b3077417c0a328e71ee4415
Ananya03 <ananya03ann@gmail.com>
2026-05-21 20:16:30 +0530

    Created Lambda handler function

--- lambda_weather.py
+++ lambda_weather.py
@@ -3,3 +3,8 @@ import urllib.request
 import boto3
 from datetime import datetime
 
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": "Lambda started"
+    }
