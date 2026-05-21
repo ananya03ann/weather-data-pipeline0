f53cbc723f25f49dda5e3a7f410d5e9823cd169c
Ananya03 <ananya03ann@gmail.com>
2026-05-21 20:20:10 +0530

    Added OpenWeather API configuration

--- lambda_weather.py
+++ lambda_weather.py
@@ -4,7 +4,13 @@ import boto3
 from datetime import datetime
 
 def lambda_handler(event, context):
+
+    api_key = "YOUR_API_KEY"
+    city = "Kozhikode"
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
     return {
         "statusCode": 200,
-        "body": "Lambda started"
+        "body": url
     }
