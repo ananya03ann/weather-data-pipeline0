6610293b4364f7b3a822da9910fc48ef9845ba4f
Ananya03 <ananya03ann@gmail.com>
2026-05-21 20:39:00 +0530

    Implemented DynamoDB stream event processing

--- lambda_stream.py
+++ lambda_stream.py
@@ -6,4 +6,9 @@ s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
 
-    return "Stream processing started"
+    for record in event['Records']:
+        if record['eventName'] == 'INSERT':
+
+            data = record['dynamodb']['NewImage']
+
+    return "Stream processed"
