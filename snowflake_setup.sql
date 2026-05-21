b436a03ed4ca730b3bb0e868acd9b4edd267554a
Ananya03 <ananya03ann@gmail.com>
2026-05-21 21:10:10 +0530

    Created Snowflake database and schema

--- snowflake_setup.sql
+++ snowflake_setup.sql
@@ -5,3 +5,12 @@ STORAGE_PROVIDER = S3
 ENABLED = TRUE
 STORAGE_AWS_ROLE_ARN = '<AWS_ROLE_ARN>'
 STORAGE_ALLOWED_LOCATIONS = ('s3://weather-data-ann/');
+
+-- Create Database and Schema
+CREATE DATABASE weather_db;
+
+USE DATABASE weather_db;
+
+CREATE SCHEMA weather_schema;
+
+USE SCHEMA weather_schema;
