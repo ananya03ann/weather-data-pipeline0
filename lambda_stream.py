d2f676ab31708fd5cec7f82f2c4eda66726ea7a0
Ananya03 <ananya03ann@gmail.com>
2026-05-21 20:40:12 +0530

    Extracted weather data from DynamoDB stream

--- lambda_stream.py
+++ lambda_stream.py
@@ -11,4 +11,11 @@ def lambda_handler(event, context):
 
             data = record['dynamodb']['NewImage']
 
+            item = {
+                "city": data['city']['S'],
+                "timestamp": data['timestamp']['S'],
+                "temperature": data['temperature']['S'],
+                "humidity": data['humidity']['S']
+            }
+
     return "Stream processed"
